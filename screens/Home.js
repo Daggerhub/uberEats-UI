5cba2aa04e7f5480b1e12d57e27709036d0f41ac
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-10-03 13:06:10 +0530

    Added search box component

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import { View, Text, StyleSheet, Platform, StatusBar, SafeAreaView } from 'react-native'
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
   <SafeAreaView style={styles.AndroidSafeArea} >
+    <View style={{backgroundColor: "white", padding: 15}}>
     <HeaderTabs/>
+    <SearchBar/>
+    </View>
   </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   AndroidSafeArea: {
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: "#eee",
+    flex: 1
   }
 });
